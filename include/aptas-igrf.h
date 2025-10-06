4224b08a858baa89042c36a7328da3c857a146ba
Björn Sundin <bjornsundin02@gmail.com>
2025-10-06 08:09:28 +0200

    Update function documentation

--- include/aptas-igrf.h
+++ include/aptas-igrf.h
@@ -25,24 +25,19 @@ typedef struct {
 static double const igrf_earth_radius = 6371.2; // [km]
 
 /**
- * Reads Schmidt quasi-normal internal spherical harmonic coefficients from a specified IGRF model file into an array
- * and extrapolates the coefficients from the date of the base model (which is set in the constant IGRF_base_year) to a given date.
+ * Reads Schmidt semi-normalized spherical harmonic coefficients from a specified IGRF model file into an array
  * \param file_name Null-terminated file name string.
- * \param extrapolated_year The date, given in decimal years, to extrapolate the model to.
- * \param coordinate_system Whether to use geodetic or geocentric coordinate system.
  * \note This function must be called before using calculate_model_geomagnetic_field.
  */
 IGRFError load_IGRF_coefficients(char const* file_name);
 
-
-// void extrapolate_IGRF_coefficients(double year);
-
 /**
- * Calculates the geomagnetic field at an ECEF point based on the IGRF model.
+ * Calculates the geomagnetic field at an ECEF point and a date given as a decimal year, based on the IGRF model.
  * \param latitude North latitude in degrees.
  * \param longitude East longitude in degrees.
- * \param altitude Given in metres.
- * \return Geomagnetic field vector where x is eastward, y is northward, and z is vertically upwards from Earth.
+ * \param altitude Given in kilometres.
+ * \param extrapolated_year The date, given in decimal years, to extrapolate the model to.
+ * \return Geomagnetic field vector.
  * \note load_IGRF_coefficients must be called before trying to use this function.
  */
 magnetic_field_vector_t calculate_model_geomagnetic_field(double latitude, double longitude, double altitude, double decimal_year);
